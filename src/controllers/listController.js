311a25e36ddd1dc363ebf078e2d0dd44228a1b9a
RemiDebruyne <remidebruyne@hotmail.com>
2024-09-18 16:02:13 +0200

    feat: add list controller

---
+++ src/controllers/listController.js
@@ -0,0 +1,58 @@
+import { db } from "../../config/db.ts";
+import { lists } from "../db/schema.ts";
+import { eq } from "drizzle-orm";
+
+export const addList = async (req, res) => {
+  try {
+    const list = req.body;
+    const result = await db
+      .insert(lists)
+      .values({ name: list.name, tableId: list.tableId });
+    // res.json(result)
+    res.json({
+      message: "List was successfuly created",
+      table: { id: result[0].insertId, name: list.name, tableId: list.tableId },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateList = async (req, res) => {
+  const idFromRoute = req.params.id;
+
+
+  const list = req.body;
+  try {
+    const result = await db
+      .update(lists)
+      .set({
+        name: list.name,
+      })
+      .where(eq(lists.id, idFromRoute));
+
+    res.json({
+      message: "list updated succesfuly",
+      table: {
+        id: idFromRoute,
+        name: list.name,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deleteList = async (req, res) => {
+  const idFromroute = req.params.id;
+  try {
+
+
+    const result = await db.delete(lists).where(eq(lists.id, idFromroute));
+    res.json({
+      message: `List with id ${idFromroute} was deleted successfuly`,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
